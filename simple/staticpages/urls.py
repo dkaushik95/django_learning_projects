b1584ce39ed83b8526424dc6028439a4cbd364aa
Dishant Kaushik <dkaushik95@gmail.com>
2017-11-26 23:08:14 -0500

    Simple app part 2

--- simple/staticpages/urls.py
+++ simple/staticpages/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from .views import home_page_view
-from .views import about_page_view
+from .views import Home_page_view, About_page_view
 
 urlpatterns = [
-    url(r'^$', home_page_view, name="home"),
-    url(r'^about', about_page_view, name="about")
+    url(r'^$', Home_page_view.as_view(), name="home"),
+    url(r'^about', About_page_view.as_view(), name="about")
 ]
